cc9116996c1d43029d176d4b700546a726fa8612
Abraham Chiu <abraham.ch.chiu@gmail.com>
2024-04-11 23:44:21 -0500

    add package1

--- scripts/increment_version.sh
+++ scripts/increment_version.sh
@@ -8,24 +8,21 @@ ls
 # Determine the type of version increment based on the commit message
 if [[ $last_commit == *"BREAKING CHANGE"* ]]; then
     # Increment major version for breaking changes
-    current_version=$(grep -oP '^version "\K\d+\.\d+\.\d+' build.gradle)
+    current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
     echo "major $current_version"
     new_version=$(echo $current_version | awk -F. '{$1++; print $1 ".0.0"}')
 elif [[ $last_commit == *"feat:"* ]]; then
     # Increment minor version for new features
-    current_version=$(grep -oP '^version "\K\d+\.\d+\.\d+' build.gradle)
+    current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
     echo "minor $current_version"
     new_version=$(echo $current_version | awk -F. '{$2++; print $1 "." $2 ".0"}')
 else
     # Increment patch version for other changes
-    current_version=$(grep -oP '^version "\K\d+\.\d+\.\d+' build.gradle)
+    current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
     echo "$current_version"
     new_version=$(echo $current_version | awk -F. '{$3++; print $1 "." $2 "." $3}')
 fi
 echo "$new_version"
-
-echo "New version: $new_version"
-
 
 # Update the version in the build.gradle file
 sed -i "s/version \"$current_version\"/version \"$new_version\"/" build.gradle
