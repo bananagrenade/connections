7a593e8a779598bc72aed02d6e10ea2846e136f0
Abraham Chiu <abraham.ch.chiu@gmail.com>
2024-04-11 23:47:15 -0500

    add package1

--- scripts/increment_version.sh
+++ scripts/increment_version.sh
@@ -8,20 +8,16 @@ last_commit=$(git log -1 --pretty=%B)
 if [[ $last_commit == *"BREAKING CHANGE"* ]]; then
     # Increment major version for breaking changes
     current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
-    echo "major $current_version"
     new_version=$(echo $current_version | awk -F. '{$1++; print $1 ".0.0"}')
 elif [[ $last_commit == *"feat:"* ]]; then
     # Increment minor version for new features
     current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
-    echo "minor $current_version"
     new_version=$(echo $current_version | awk -F. '{$2++; print $1 "." $2 ".0"}')
 else
     # Increment patch version for other changes
     current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
-    echo "$current_version"
     new_version=$(echo $current_version | awk -F. '{$3++; print $1 "." $2 "." $3}')
 fi
-echo "$new_version"
 
 # Update the version in the build.gradle file
 sed -i "s/version \"$current_version\"/version \"$new_version\"/" build.gradle
