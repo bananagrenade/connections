e1319a0db3897dc49c5d5768ac83c2217ae105d1
Abraham Chiu <abraham.ch.chiu@gmail.com>
2024-04-16 17:15:59 -0500

    BREAKING CHANGE: add package3

--- scripts/increment_version.sh
+++ scripts/increment_version.sh
@@ -5,17 +5,16 @@ last_commit=$(git log -1 --pretty=%B)
 #!/bin/bash
 
 # Determine the type of version increment based on the commit message
+current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
 if [[ $last_commit == *"BREAKING CHANGE"* ]]; then
     # Increment major version for breaking changes
-    current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
-    new_version=$(echo $current_version | awk -F. '{$1++; print $1 ".0.0"}')
+    new_version=$(echo "$current_version" | awk -F. '{$1++; print $1 ".0.0"}')
 elif [[ $last_commit == *"feat:"* ]]; then
     # Increment minor version for new features
-    current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
-    new_version=$(echo $current_version | awk -F. '{$2++; print $1 "." $2 ".0"}')
+    new_version=$(echo "$current_version" | awk -F. '{$2++; print $1 "." $2 ".0"}')
 else
     # Increment patch version for other changes
-    current_version=$(grep -oP "version\s*=\s*'\K\d+\.\d+\.\d+" build.gradle)
+    # shellcheck disable=SC2086
     new_version=$(echo $current_version | awk -F. '{$3++; print $1 "." $2 "." $3}')
 fi
 
